8d9d298b17a021c173199d4425ce4fd45963c097
spark1031 <sujinpark1031@gmail.com>
2018-11-23 14:54:27 -0800

    Add bubble sort completed

--- intro_to_callbacks_exercises/absurd_bubble_sort.js
+++ intro_to_callbacks_exercises/absurd_bubble_sort.js
@@ -1,4 +1,57 @@
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}?`, (res) => {
+    res === 'yes' ? callback(true) : callback(false);
+  });
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i === arr.length - 1) {
+    outerBubbleSortLoop(madeAnySwaps);
+    return;
+  }
+  askIfGreaterThan(arr[i], arr[i+1], (isGreaterThan) => {
+    if (isGreaterThan) {
+      madeAnySwaps = true;
+      let num = arr[i];
+      let next_num = arr[i + 1];
+      arr[i] = next_num;
+      arr[i+1] = num;
+    }
+    innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+  });
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop (madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
+}
+
+function callback1(arr) {
+  console.log(arr);
+  return;
+}
+
+absurdBubbleSort([3, 2, 1], callback1);
+
+// 
+// innerBubbleSortLoop([3,2,1], 0, false);
+// 
+// askIfGreaterThan(3, 2, (boolean) => {
+//   console.log(boolean);
+// });
 
 
 
