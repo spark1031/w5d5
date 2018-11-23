a950bfe2d040d3568c18cd54cd022b736a51375e
spark1031 <sujinpark1031@gmail.com>
2018-11-23 12:29:32 -0800

    Add timing.js solution for callback exercises

---
+++ timing.js
@@ -0,0 +1,58 @@
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    let ourDate = new Date();
+    // 2. Store the hours, minutes, and seconds.
+    this.hours = ourDate.getHours();
+    this.minutes = ourDate.getMinutes();
+    this.seconds = ourDate.getSeconds();
+    // 3. Call printTime.
+    this.printTime();
+    
+    // 4. Schedule the tick at 1 second intervals.
+    setInterval(this._tick.bind(this), 1000);
+    
+    // this = clock object with time set to when we create new Date object
+    
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    // 2. Call printTime.
+    
+    // this = previous clock object (with time of previous second)
+    
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.update_minutes();
+    }
+    
+    // this = updated clock object (with current time)
+    
+    this.printTime(); // prints the time of the updated clock object
+  }
+  
+  update_minutes() {
+    this.minutes += 1;
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.update_hour();
+    }
+  }
+  
+  update_hour() {
+    this.hour += 1;
+    if (this.hour === 13) {
+      this.hour = 1;
+    }
+  }
+}
+
+const clock = new Clock();
